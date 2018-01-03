ea2cbed854896c108a896cfe6985ff3ef65f88ba
syt123450 <421537891@qq.com>
2018-01-02 16:14:51 -0800

    add structure

--- globe.js
+++ globe.js
@@ -1,3 +1,47 @@
 /**
  * Created by ss on 2018/1/2.
  */
+Globe = function(container) {
+
+    this.container = container;
+
+    this.addData = function() {
+
+    };
+
+    this.init = function() {
+
+    };
+
+    function onClick() {
+        clickCountry();
+    }
+
+    function clickCountry() {
+
+        //update parameters
+
+        highlightCountry();
+        generateLines();
+        generatePartial();
+        initMarker();
+    }
+
+    function highlightCountry() {
+
+    }
+
+    function generateLines() {
+
+    }
+
+    function generatePartial() {
+
+    }
+
+    function initMarker() {
+
+    }
+
+
+};

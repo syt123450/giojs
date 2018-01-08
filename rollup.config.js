39d51420ea685d0cf7035ef93ff23c212317273e
syt123450 <421537891@qq.com>
2018-01-07 16:20:03 -0800

    init basic build process

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+/**
+ * Created by ss on 2018/1/7.
+ */
+export default {
+    input: 'src/IOGlobe.js',
+    output: {
+        format: 'iife',
+        file: 'build/IOGlobe.min.js',
+        name: "IOGlobe"
+    }
+};
---
+++ src/IOGlobe.js
@@ -0,0 +1,9 @@
+/**
+ * Created by ss on 2018/1/7.
+ */
+
+var Scene = function() {
+
+};
+
+export {Scene}

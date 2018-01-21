cc6f6a99fd0f4da469b075ad51e613eddedaea95
Mokuten OH <mokuteno@gmail.com>
2018-01-21 15:10:04 -0800

    modify AbstractDataProcessor

--- src/dataPreprocessors/AbstractDataProcessor.js
+++ src/dataPreprocessors/AbstractDataProcessor.js
@@ -1,7 +1,3 @@
-/**
- * @author mokuteno / https://github.com/manymeeting
- */
-
 AbstractDataProcessor = function() {
     this.successor = null;
 };
@@ -9,7 +5,22 @@ AbstractDataProcessor = function() {
 AbstractDataProcessor.prototype.setSuccessor = function(dp) {
     this.successor = dp;
 }
-AbstractDataProcessor.prototype.process = function(data) {
+
+AbstractDataProcessor.prototype.process = function(controller) {
+	if(this.isMatched(controller))
+	{
+		this.processDetail(controller);
+	}
+	
+	this.successor.process(controller);
 }
+
+AbstractDataProcessor.prototype.processDetail = function(controller) {
+}
+
+AbstractDataProcessor.prototype.isMatched = function(controller) {
+	return true;
+}
+
 
 export { AbstractDataProcessor }

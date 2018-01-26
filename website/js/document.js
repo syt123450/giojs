7226af07b4d616409cd8ed479a37e3405adcb017
syt123450 <421537891@qq.com>
2018-01-26 05:58:15 -0800

    optimize nav bar

--- website/js/document.js
+++ website/js/document.js
@@ -2,4 +2,8 @@ $(function() {
     $("#more").click(function() {
         $("#nav-collapse").slideToggle();
     });
-})
+
+    $("#logo").click(function() {
+        window.location.href = "../index.html";
+    });
+});

c3c6ab03a3122ecbd0b5199356ef4225d1498d60
syt123450 <421537891@qq.com>
2018-01-28 04:16:13 -0800

    add document nav and list items

--- website/js/document.js
+++ website/js/document.js
@@ -1,9 +1,63 @@
 $(function() {
+
+    var selectedUl;
+
     $("#more").click(function() {
         $("#nav-collapse").slideToggle();
     });
 
     $("#logo").click(function() {
         window.location.href = "../index.html";
+    });
+
+    $("#startLabel").click(function() {
+
+        if (selectedUl !== "#start") {
+            $(selectedUl).slideUp();
+        }
+
+        selectedUl = "#start";
+        $("#start").slideToggle();
+    });
+
+    $("#basicLabel").click(function() {
+
+        if (selectedUl !== "#basic") {
+            $(selectedUl).slideUp();
+        }
+
+        selectedUl = "#basic";
+        $("#basic").slideToggle();
+    });
+
+    $("#designLabel").click(function() {
+
+        if (selectedUl !== "#design") {
+            $(selectedUl).slideUp();
+        }
+
+        selectedUl = "#design";
+        $("#design").slideToggle();
+    });
+
+    $("#dataLabel").click(function() {
+
+        if (selectedUl !== "#data") {
+            $(selectedUl).slideUp();
+        }
+
+        selectedUl = "#data";
+        $("#data").slideToggle();
+    });
+
+    $("#advancedLabel").click(function() {
+
+        if (selectedUl !== "#advanced") {
+            $(selectedUl).slideUp();
+        }
+
+        selectedUl = "#advanced";
+
+        $("#advanced").slideToggle();
     });
 });

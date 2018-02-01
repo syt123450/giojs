704fdacbf0b21d475f831c46823acae456b662ee
Mokuten OH <mokuteno@gmail.com>
2018-01-31 17:50:27 -0800

    update unit test config

--- test/unit/src/utils/Utils.tests.js
+++ test/unit/src/utils/Utils.tests.js
@@ -1,9 +1,29 @@
 import { Utils } from '../../../../src/utils/Utils';
 import assert from 'assert';
+import chai from "chai"
 
 describe('Utils', function() {
     it('formatColor()', function() {
         assert.equal(0xffffff, Utils.formatColor('#ffffff'));
         assert.equal(0xffffff, Utils.formatColor(0xffffff));
     });
+});
+
+describe('Test constrain', function () {
+
+	var min = 10;
+	var max = 100;
+
+	it('should return min for input < min', function () {
+		chai.expect(Utils.constrain(9, min, max)).to.equal(min);
+	});
+
+	it('should return max for input > max', function () {
+		chai.expect(Utils.constrain(101, min, max)).to.equal(max);
+	});
+
+	it('should return value for min < value < max', function () {
+		chai.expect(Utils.constrain(11, min, max)).to.equal(11);
+	});
+  
 });

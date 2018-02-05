b43c26263b316898757671b2276745eb6d986622
syt123450 <421537891@qq.com>
2018-02-05 10:55:12 -0800

    establish basic structure for document

--- website/js/document.js
+++ website/js/document.js
@@ -1,6 +1,7 @@
-$(function() {
+var selectedNav;
+var selectedArea;
 
-    var selectedUl;
+$(function() {
 
     $("#more").click(function() {
         $("#nav-collapse").slideToggle();
@@ -10,85 +11,133 @@ $(function() {
         window.location.href = "../index.html";
     });
 
-    $("#startLabel").click(function() {
-
-        if (selectedUl !== "#start") {
-            $(selectedUl).slideUp();
-        }
-
-        selectedUl = "#start";
-        $("#start").slideToggle();
-    });
-
-    $("#basicLabel").click(function() {
-
-        if (selectedUl !== "#basic") {
-            $(selectedUl).slideUp();
-        }
-
-        selectedUl = "#basic";
-        $("#basic").slideToggle();
-    });
-
-    $("#configureLable").click(function() {
-
-        if (selectedUl !== "#configure") {
-            $(selectedUl).slideUp();
-        }
-
-        selectedUl = "#configure";
-        $("#configure").slideToggle();
-    });
-
-    $("#designLabel").click(function() {
-
-        if (selectedUl !== "#design") {
-            $(selectedUl).slideUp();
-        }
-
-        selectedUl = "#design";
-        $("#design").slideToggle();
-    });
-
-    $("#colorLabel").click(function() {
-
-        if (selectedUl !== "#color") {
-            $(selectedUl).slideUp();
-        }
-
-        selectedUl = "#color";
-        $("#color").slideToggle();
-    });
-
-    $("#dataLabel").click(function() {
-
-        if (selectedUl !== "#data") {
-            $(selectedUl).slideUp();
-        }
-
-        selectedUl = "#data";
-        $("#data").slideToggle();
-    });
-
-    $("#callbackLabel").click(function() {
-
-        if (selectedUl !== "#callback") {
-            $(selectedUl).slideUp();
-        }
-
-        selectedUl = "#callback";
-        $("#callback").slideToggle();
-
-    });
-
-    $("#advancedLabel").click(function() {
-
-        if (selectedUl !== "#advanced") {
-            $(selectedUl).slideUp();
-        }
-
-        selectedUl = "#advanced";
-
-        $("#advanced").slideToggle();
-    });
+    bindDocumentEvent();
 });
+
+function bindDocumentEvent() {
+
+    bindStartChapter();
+    bindConceptChapter();
+    bindConfigureChapter();
+    bindDesignChapter();
+    bindColorChapter();
+    bindDataChapter();
+    bindCallbackChapter();
+    bindAdvancedChapter();
+
+}
+
+function bindStartChapter() {
+
+    bindChapterHelper("#startLabel", "#start", "#startGuide");
+    bindSectionHelper("#requirementNav", "#requirement");
+    bindSectionHelper("#installNav", "#install");
+    bindSectionHelper("#firstGlobeNav", "#firstGlobe");
+    bindSectionHelper("#explainLastNav", "#explainLast");
+
+}
+
+function bindConceptChapter() {
+
+    bindChapterHelper("#basicLabel", "#basic", "#conceptGuide");
+    bindSectionHelper("#globeNav", "#globe");
+    bindSectionHelper("#surfaceNav", "#surface");
+    bindSectionHelper("#lineNav", "#line");
+    bindSectionHelper("#backgroundNav", "#background");
+    bindSectionHelper("#haloNav", "#halo");
+    bindSectionHelper("#countryNav", "#country");
+    bindSectionHelper("#oceanNav", "#ocean");
+
+}
+
+function bindConfigureChapter() {
+
+    bindChapterHelper("#configureLabel", "#configure", "#configureGuide");
+    bindSectionHelper("#constructorConfigureNav", "#constructorConfigure");
+    bindSectionHelper("#configureAPINav", "#configureAPI");
+
+}
+
+function bindDesignChapter() {
+
+    bindChapterHelper("#designLabel", "#design", "#designGuide");
+    bindSectionHelper("#setInitCountryNav", "#setInitCountry");
+    bindSectionHelper("#lightenMentionedNav", "#lightenMentioned");
+    bindSectionHelper("#disableUnmentionedNav", "#disableUnmentioned");
+    bindSectionHelper("#showOnlyNav", "#showOnly");
+    bindSectionHelper("#controlHaloNav", "#controlHalo");
+    bindSectionHelper("#controlStatsNav", "#controlStats");
+
+}
+
+function bindColorChapter() {
+
+    bindChapterHelper("#colorLabel", "#color", "#colorGuide");
+    bindSectionHelper("#setStyleNav", "#setStyle");
+    bindSectionHelper("#setSurfaceColorNav", "#setSurfaceColor");
+    bindSectionHelper("#setSelectedColorNav", "#setSelectedColor");
+    bindSectionHelper("#setExportColorNav", "#setExportColor");
+    bindSectionHelper("#setImportColorNav", "#setImportColor");
+    bindSectionHelper("#setHaloColorNav", "#setHaloColor");
+    bindSectionHelper("#setBackgroundColorNav", "#setBackgroundColor");
+    bindSectionHelper("#adjustOceanBrightnessNav", "#adjustOceanBrightness");
+    bindSectionHelper("#adjustRelatedBrightnessNav", "#adjustRelatedBrightness");
+    bindSectionHelper("#adjustMentionedBrightnessNav", "#adjustMentionedBrightness");
+
+}
+
+function bindDataChapter() {
+
+    bindChapterHelper("#dataLabel", "#data", "#dataGuide");
+    bindSectionHelper("#addDataNav", "#addData");
+    bindSectionHelper("#addDataAsyncNav", "#addDataAsync");
+    bindSectionHelper("#liveLoadNav", "#liveLoad");
+    bindSectionHelper("#setDataColorNav", "#setFromData");
+
+}
+
+function bindCallbackChapter() {
+
+    bindChapterHelper("#callbackLabel", "#callback", "#callbackGuide");
+    bindSectionHelper("#onCountryPickedNav", "#onCountryPicked");
+
+}
+
+function bindAdvancedChapter() {
+
+    bindChapterHelper("#advancedLabel", "#advanced", "#advancedGuide");
+    bindSectionHelper("#functionChainNav", "#functionChain");
+    bindSectionHelper("#switchCountryNav", "#switchCountry");
+
+}
+
+function bindChapterHelper(chapterLabel, chapterSelection, chapterGuideArea) {
+
+    $(chapterLabel).click(function() {
+
+        if (selectedNav !== chapterSelection) {
+            $(selectedNav).slideUp();
+        }
+
+        selectedNav = chapterSelection;
+
+        $(chapterSelection).slideToggle();
+
+        $(selectedArea).hide();
+
+        selectedArea = chapterGuideArea;
+        $(selectedArea).show();
+    });
+
+}
+
+function bindSectionHelper(sectionNav, sectionArea) {
+
+    $(sectionNav).click(function() {
+
+        $(selectedArea).hide();
+        selectedArea = sectionArea;
+        $(selectedArea).show();
+
+    });
+}

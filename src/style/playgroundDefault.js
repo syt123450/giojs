cb60614b39fc2e1805c8f1cace771e72e3c66560
Qi Liu <liuqi0087@gmail.com>
2018-02-12 15:13:19 -0800

    Add style file indexDefault, playgroundDefault and yellow

---
+++ src/style/playgroundDefault.js
@@ -0,0 +1,39 @@
+/**
+ * @author Qi Liu / https://github.com/lq3297401
+ */
+
+// Developer can adjust and define their own style like this, and contribute to our repository
+
+var playgroundDefault = ( function () {
+
+    return {
+
+        color: {
+
+            surface: 0x29ABEF,
+
+            selected: 0x29ABEF,
+
+            in: 0x0AFF1B,
+
+            out: 0xFFA114,
+
+            halo: 0xB5F3FF,
+
+            background: 0x000000
+        },
+
+        brightness: {
+
+            ocean: 0.5,
+
+            mentioned: 0.5,
+
+            related: 0.5
+        }
+
+    }
+
+}() );
+
+export { playgroundDefault }
---
+++ src/style/yellow.js
@@ -0,0 +1,39 @@
+/**
+ * @author Qi Liu / https://github.com/lq3297401
+ */
+
+// Developer can adjust and define their own style like this, and contribute to our repository
+
+var yellow = ( function () {
+
+    return {
+
+        color: {
+
+            surface: 0xCC6600,
+
+            selected: 0xFFFF00,
+
+            in: 0xF70FFF,
+
+            out: 0xFF0D3D,
+
+            halo: 0xFFFF00,
+
+            background: 0x000000
+        },
+
+        brightness: {
+
+            ocean: 0.5,
+
+            mentioned: 0.5,
+
+            related: 0.5
+        }
+
+    }
+
+}() );
+
+export { yellow }

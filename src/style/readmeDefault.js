954ef789f0bf9c6b8b049ad1ffa6cd7c32485d99
Qi Liu <liuqi0087@gmail.com>
2018-02-13 17:07:27 -0800

    Add readmeDefault style

---
+++ src/style/readmeDefault.js
@@ -0,0 +1,39 @@
+/**
+ * @author Qi Liu / https://github.com/lq3297401
+ */
+
+// Developer can adjust and define their own style like this, and contribute to our repository
+
+var readmeDefault = ( function () {
+
+    return {
+
+        color: {
+
+            surface: 0x48C120,
+
+            selected: 0xF17F49,
+
+            in: 0x0091FF,
+
+            out: 0xFF8000,
+
+            halo: 0x2C4247,
+
+            background: 0x1A2029
+        },
+
+        brightness: {
+
+            ocean: 0.5,
+
+            mentioned: 0.5,
+
+            related: 0.5
+        }
+
+    }
+
+}() );
+
+export { readmeDefault }

f3696319d51b9ead1180172c6ebe0e1939bad18a
syt123450 <421537891@qq.com>
2018-02-14 02:41:57 -0800

    update developer page

--- js/developer.js
+++ js/developer.js
@@ -1,3 +1,19 @@
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 650 || document.documentElement.scrollTop > 650) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
 $(function() {
     $("#more").click(function() {
         $("#nav-collapse").slideToggle();
@@ -9,7 +25,7 @@ $(function() {
 
     $("#navigation1").click(function () {
         $('html, body').animate({
-            scrollTop: $("#setup").offset().top
+            scrollTop: $("#pullRequest").offset().top
         }, 1000);
     });
 
@@ -21,7 +37,41 @@ $(function() {
 
     $("#navigation3").click(function () {
         $('html, body').animate({
-            scrollTop: $("#pullRequest").offset().top
+            scrollTop: $("#setup").offset().top
         }, 1000);
     });
+
+
+    $("#navigation1").hover(
+        function() {
+            $("#navigation1 img").attr("src", "../assets/images/goPullRequest_hover.png");
+            $("#navigation1 p").css({color:'#29abe2'});
+        },
+        function() {
+            $("#navigation1 img").attr("src", "../assets/images/goPullRequest.png");
+            $("#navigation1 p").css({color:'#0071bc'});
+        }
+    );
+
+    $("#navigation2").hover(
+        function() {
+            $("#navigation2 img").attr("src", "../assets/images/goStructure_hover.png");
+            $("#navigation2 p").css({color:'#29abe2'});
+        },
+        function() {
+            $("#navigation2 img").attr("src", "../assets/images/goStructure.png");
+            $("#navigation2 p").css({color:'#0071bc'});
+        }
+    );
+
+    $("#navigation3").hover(
+        function() {
+            $("#navigation3 img").attr("src", "../assets/images/goSetup_hover.png");
+            $("#navigation3 p").css({color:'#29abe2'});
+        }, function() {
+            $("#navigation3 img").attr("src", "../assets/images/goSetup.png");
+            $("#navigation3 p").css({color:'#0071bc'});
+        }
+    );
+
 });

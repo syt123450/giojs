632792d7e2967f0013b7773aeed74020c0e16024
syt123450 <421537891@qq.com>
2018-02-14 23:39:19 -0800

    update gio build folder for release

--- build/gio.module.js
+++ build/gio.module.js
@@ -1,7 +1,7 @@
 (function (global, factory) {
 	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
 	typeof define === 'function' && define.amd ? define(['exports'], factory) :
-	(factory((global['GIO-umd'] = {})));
+	(factory((global.GIO = {})));
 }(this, (function (exports) { 'use strict';
 
 global.THREE = require("three");

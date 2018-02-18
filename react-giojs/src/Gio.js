01566de89176c52c11fe97987180ea0b11709e49
syt123450 <421537891@qq.com>
2018-02-17 18:37:54 -0800

    init react-giojs project

---
+++ react-giojs/src/Gio.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Gio extends Component {
+
+    render() {
+
+        return (
+            <div className="globe">Hello Gio
+            </div>
+        );
+    }
+}
+
+export default Gio;

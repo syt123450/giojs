9fda55c61a771853647e3f8ae4168ee99aa8aec4
siriustom <linxiaoran0210@gmail.com>
2018-02-22 22:19:40 -0800

    wrap Gio in react component plus some supporting files

--- react-giojs/test/js/App.js
+++ react-giojs/test/js/App.js
@@ -1,45 +1,57 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import Gio from '../../src/Gio.js';
+import { countryData } from '/Users/Tom/Documents/Gio.js/react-giojs/test/data/TestData.js';
+import Colors from '/Users/Tom/Documents/Gio.js/react-giojs/utils/Colors.js';
 
-var data = [
-    {
-        "e": "CN",
-        "i": "US",
-        "v": 3300000
-    },
-    {
-        "e": "CN",
-        "i": "RU",
-        "v": 10000
-    },
-    {
-        "e": "RU",
-        "i": "US",
-        "v": 3123
-    },
-    {
-        "e": "FR",
-        "i": "CN",
-        "v": 2
-    },
-    {
-        "e": "US",
-        "i": "FR",
-        "v": 5
-    }
-];
+// var data = [
+//     {
+//         "e": "CN",
+//         "i": "US",
+//         "v": 3300000
+//     },
+//     {
+//         "e": "CN",
+//         "i": "RU",
+//         "v": 10000
+//     },
+//     {
+//         "e": "RU",
+//         "i": "US",
+//         "v": 3123
+//     },
+//     {
+//         "e": "FR",
+//         "i": "CN",
+//         "v": 2
+//     },
+//     {
+//         "e": "US",
+//         "i": "FR",
+//         "v": 5
+//     }
+// ];
 
 class App extends Component {
 
-    componentDidMount() {
-        axios.get(`./data/countryData.json`)
-            .then(response => console.log(response.data))
+    constructor() {
+        super();
+
+        this.state = { surfaceColor: Colors.white };
+
+        setTimeout(() => {
+            this.setState({ surfaceColor: Colors.blue });
+        }, 3000)
     }
+
+    // componentDidMount() {
+    //     axios.get(`./data/countryData.json`)
+    //         .then(response => console.log(response.data))
+    // }
 
     render() {
         return (
-            <Gio data={data} />
+            <Gio data={countryData} />
         )
     }
 }
---
+++ react-giojs/utils/Colors.js
@@ -0,0 +1,8 @@
+const Colors = {
+    red: '#FB011E',
+    white: '#FFFFFF',
+    green: '#01FB51',
+    blue: '#0111FB',
+}
+
+export default Colors;

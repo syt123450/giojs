31d8cb1406fdb051244a218029b45f3ce38b389c
syt123450 <421537891@qq.com>
2018-03-19 01:38:25 -0700

    update default props

--- react-giojs/src/constants/DefaultGioProps.js
+++ react-giojs/src/constants/DefaultGioProps.js
@@ -1,17 +1,9 @@
-/**
- * This is the default properties of Gio which is not defined in original Giojs library
- * Used as an static object
- */
 
 var DefaultGioProps = (function() {
 
     return {
 
-        // width of container
-
         width: "100%",
-
-        // height of container
 
         height: "100%",
 

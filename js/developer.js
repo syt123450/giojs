feff9f14ea139a387c37ea39a5e98ce2475d4304
Qi Liu <liuqi0087@gmail.com>
2018-04-18 22:57:16 -0700

    Add ripple effect to Top button

--- js/developer.js
+++ js/developer.js
@@ -1,3 +1,35 @@
+(function (window, $) {
+
+    $(function() {
+
+        $('.ripple').on('click', function (event) {
+            event.preventDefault();
+
+            var $div = $('<div/>'),
+                btnOffset = $(this).offset(),
+                xPos = event.pageX - btnOffset.left,
+                yPos = event.pageY - btnOffset.top;
+
+            $div.addClass('ripple-effect');
+            var $ripple = $(".ripple-effect");
+
+            $ripple.css("height", $(this).height());
+            $ripple.css("width", $(this).height());
+            $div
+                .css({
+                    top: yPos - ($ripple.height()/2),
+                    left: xPos - ($ripple.width()/2),
+                    background: $(this).data("ripple-color")
+                })
+                .appendTo($(this));
+
+            window.setTimeout(function(){
+                $div.remove();
+            }, 2000);
+        });
+    });
+})(window, jQuery);
+
 $(document).ready(function(){
     $("#myBtn").hide();
 

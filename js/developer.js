c7ad393cf18c33fd7787bd64c1546e06229b28a5
syt123450 <421537891@qq.com>
2018-04-21 18:28:11 -0700

    fix hover color not change bug

--- js/developer.js
+++ js/developer.js
@@ -1,57 +1,3 @@
-(function (window, $) {
-
-    $(function() {
-
-        $('.ripple').on('click', function (event) {
-            event.preventDefault();
-
-            var $div = $('<div/>'),
-                btnOffset = $(this).offset(),
-                xPos = event.pageX - btnOffset.left,
-                yPos = event.pageY - btnOffset.top;
-
-            $div.addClass('ripple-effect');
-            var $ripple = $(".ripple-effect");
-
-            $ripple.css("height", $(this).height());
-            $ripple.css("width", $(this).height());
-            $div
-                .css({
-                    top: yPos - ($ripple.height()/2),
-                    left: xPos - ($ripple.width()/2),
-                    background: $(this).data("ripple-color")
-                })
-                .appendTo($(this));
-
-            window.setTimeout(function(){
-                $div.remove();
-            }, 2000);
-        });
-    });
-})(window, jQuery);
-
-$(document).ready(function(){
-    $("#myBtn").hide();
-
-    $(function () {
-        $(window).scroll(function () {
-            if ($(this).scrollTop() > 650) {
-                $('#myBtn').fadeIn();
-            } else {
-                $('#myBtn').fadeOut();
-            }
-        });
-
-        $('#myBtn').click(function () {
-            $('body,html').animate({
-                scrollTop: 0
-            }, 800);
-            return false;
-        });
-    });
-
-});
-
 $(function() {
     $("#more").click(function() {
         $("#nav-collapse").slideToggle();
@@ -100,5 +46,64 @@ $(function() {
             $("#navigation3 p").css({color:'#0071bc'});
         }
     );
+
+	$("#myBtn").hover(function() {
+	    $(this).css({
+            "background-color": "#23bffe",
+            "color": "white"
+	    })
+    }, function () {
+		$(this).css({
+            "background-color": "rgba(242, 242, 242, 0.5)",
+		    "color": "#b3b3b3"
+		})
+    }).click(function () {
+
+		$('#myBtn').css({
+			"background-color": "#23bffe",
+			"color": "white"
+		});
+
+		$('body,html').animate({
+			scrollTop: 0
+		}, 800, function() {
+			$('#myBtn').css({
+				"background-color": "rgba(242, 242, 242, 0.5)",
+				"color": "#b3b3b3"
+			});
+		});
+	});
+
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 650) {
+			$('#myBtn').fadeIn();
+		} else {
+			$('#myBtn').fadeOut();
+		}
+	});
+
+    $('.ripple').on('click', function (event) {
+        event.preventDefault();
+
+        var $div = $('<div/>'),
+            btnOffset = $(this).offset(),
+            xPos = event.pageX - btnOffset.left,
+            yPos = event.pageY - btnOffset.top;
+
+        $div.addClass('ripple-effect');
+        var $ripple = $(".ripple-effect");
+
+        $ripple.css("height", $(this).height());
+        $ripple.css("width", $(this).height());
+        $div.css({
+            top: yPos - ($ripple.height()/2),
+            left: xPos - ($ripple.width()/2),
+            background: $(this).data("ripple-color")
+        }).appendTo($(this));
+
+        window.setTimeout(function(){
+            $div.remove();
+        }, 2000);
+    });
 
 });

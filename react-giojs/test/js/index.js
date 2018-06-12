0a2cc94ee243db6c42a71bb1a446aec61bdb8281
syt123450 <421537891@qq.com>
2018-06-12 17:03:42 +0800

    seperate react-giojs into a new repo

--- react-giojs/test/js/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import '../css/index.css';
-import App from './App';
-
-const root = document.getElementById('root');
-
-ReactDOM.render(<App />, root);

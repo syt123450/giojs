810acd8d8d20bb66775ac6fb27e50f722c2f42c2
Qi Liu <liuqi0087@gmail.com>
2019-04-02 01:47:43 -0700

    update missing styles

---
+++ src/style/FrozenBerryStyle.js
@@ -0,0 +1,37 @@
+/**
+ * @author Qi Liu / https://github.com/lq3297401
+ */
+
+var FrozenBerryStyle = ( function () {
+
+    return {
+
+        color: {
+
+            surface: 0xE8198B,
+
+            selected: 0x81F5FD,
+
+            in: 0xE8198B,
+
+            out: 0x81F5FD,
+
+            halo: 0x81F5FD,
+
+            background: 0x000000
+        },
+
+        brightness: {
+
+            ocean: 0.5,
+
+            mentioned: 0.5,
+
+            related: 0.5
+        }
+
+    }
+
+}() );
+
+export { FrozenBerryStyle }
---
+++ src/style/OctoberPartyStyle.js
@@ -0,0 +1,37 @@
+/**
+ * @author Qi Liu / https://github.com/lq3297401
+ */
+
+var OctoberPartyStyle = ( function () {
+
+    return {
+
+        color: {
+
+            surface: 0x1724D4,
+
+            selected: 0x8D4AF0,
+
+            in: 0xDCE31E,
+
+            out: 0x3B1BDE,
+
+            halo: 0xEEEE7E,
+
+            background: 0x000000
+        },
+
+        brightness: {
+
+            ocean: 0.5,
+
+            mentioned: 0.5,
+
+            related: 0.5
+        }
+
+    }
+
+}() );
+
+export { OctoberPartyStyle }

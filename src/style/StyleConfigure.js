f2b96fd595744a8771738a5e2b5cf3b1c1b59415
Qi Liu <liuqi0087@gmail.com>
2019-04-02 01:40:15 -0700

    Register styles

--- src/style/StyleConfigure.js
+++ src/style/StyleConfigure.js
@@ -6,11 +6,35 @@ import { StyleFactory } from "./StyleFactory.js";
 
 // developer need to register their new style to StyleFactory here
 
+import { BlueInkStyle } from "./BlueInkStyle";
+StyleFactory.register( "blueInk", BlueInkStyle );
+
+import { EarlySpringStyle } from "./EarlySpringStyle.js";
+StyleFactory.register( "earlySpring", EarlySpringStyle );
+
+import { FrozenBerryStyle } from "./FrozenBerryStyle.js";
+StyleFactory.register( "frozenBerry", FrozenBerryStyle );
+
+import { GorgeousDreamStyle } from "./GorgeousDreamStyle.js";
+StyleFactory.register( "gorgeousDream", GorgeousDreamStyle );
+
+import { JuicyCakeStyle } from "./JuicyCakeStyle.js";
+StyleFactory.register( "juicyCake", JuicyCakeStyle );
+
+import { LemonGateStyle } from "./LemonGateStyle.js";
+StyleFactory.register( "lemonGate", LemonGateStyle );
+
 import { MagicStyle } from "./MagicStyle.js";
 StyleFactory.register( "magic", MagicStyle );
 
 import { MintStyle } from "./MintStyle.js";
 StyleFactory.register( "mint", MintStyle );
+
+import { NearMoonStyle } from "./NearMoonStyle.js";
+StyleFactory.register( "nearMoon", NearMoonStyle );
+
+import { OctoberPartyStyle } from "./OctoberPartyStyle.js";
+StyleFactory.register( "octoberParty", OctoberPartyStyle );
 
 import { RedBlueStyle } from "./RedBlueStyle.js";
 StyleFactory.register( "redBlue", RedBlueStyle );
@@ -21,16 +45,10 @@ StyleFactory.register( "strawberry", StrawberryStyle );
 import { SunsetStyle } from "./SunsetStyle.js";
 StyleFactory.register( "sunset", SunsetStyle );
 
-import { NearMoonStyle } from "./NearMoonStyle.js";
-StyleFactory.register( "nearMoon", NearMoonStyle );
 
-import { LemonGateStyle } from "./LemonGateStyle.js";
-StyleFactory.register( "lemonGate", LemonGateStyle );
 
-import { JuicyCakeStyle } from "./JuicyCakeStyle.js";
-StyleFactory.register( "juicyCake", JuicyCakeStyle );
 
-import { GorgeousDreamStyle } from "./GorgeousDreamStyle.js";
-StyleFactory.register( "gorgeousDream", GorgeousDreamStyle );
+
+
 
 
